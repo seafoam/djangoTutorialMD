b649422e87df84e80d16de7ab4f7bdf457fc12a1
Lawrence Chan <lawrence@openroad.ca>
2014-06-12 15:13:29 -0700

    Implemented post pages

--- blogengine/admin.py
+++ blogengine/admin.py
@@ -1,5 +1,8 @@
 import models
 from django.contrib import admin
 
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
 # Register your models here.
 admin.site.register(models.Post)
