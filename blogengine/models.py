b6ffac6161648b8bbad83d3021d0955196d6e3a4
Lawrence Chan <lawrence@openroad.ca>
2014-06-13 15:40:02 -0700

    Impemented Facebook comments

--- blogengine/models.py
+++ blogengine/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.sites.models import Site
 
-# Create your models here
+# Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
     text = models.TextField()
     slug = models.SlugField(max_length=40, unique=True)
     author = models.ForeignKey(User)
+    site = models.ForeignKey(Site)
 
     def get_absolute_url(self):
         return "/%s/%s/%s/" % (self.pub_date.year, self.pub_date.month, self.slug)
