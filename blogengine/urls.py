41e64a0b835e110b429a6463e075df0b2840e528
Lawrence Chan <lawrence@openroad.ca>
2014-06-16 15:33:36 -0700

    Categories are now shown

--- blogengine/urls.py
+++ blogengine/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
 from django.views.generic import ListView, DetailView
-from blogengine.models import Post
+from blogengine.models import Post, Category
+from blogengine.views import CategoryListView
 
 urlpatterns = patterns('',
     # Index
@@ -8,8 +9,15 @@ urlpatterns = patterns('',
         model=Post,
         paginate_by=5,
         )),
+
     # Individual posts
     url(r'^(?P<pub_date__year>\d{4})/(?P<pub_date__month>\d{1,2})/(?P<slug>[a-zA-Z0-9-]+)/?$', DetailView.as_view(
         model=Post,
+        )),
+
+    # Categories
+    url(r'^category/(?P<slug>[a-zA-Z0-9-]+)/?$', CategoryListView.as_view(
+        paginate_by=5,
+        model=Category,
         )),
 )
